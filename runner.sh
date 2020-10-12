ef3fd4679ff6f94171162222efa47a2cda365215
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-12 19:21:30 +0200

    run headless

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/galaxyproject/galaxy.git --branch release_20.09 --single-branch ./dist && cd dist/
-GALAXY_TEST_EXTERNAL=https://usegalaxy.eu/ GALAXY_TEST_SELENIUM_HEADLESS=0 ./run_tests.sh -selenium lib/galaxy_test/selenium/test_uploads.py:UploadsTestCase.test_upload_simplest
+GALAXY_TEST_EXTERNAL=https://usegalaxy.eu/ ./run_tests.sh -selenium lib/galaxy_test/selenium/test_uploads.py:UploadsTestCase.test_upload_simplest
