cc655e69656ea79c52654d5c9a493e9dbf86ace1
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-13 16:39:23 +0200

    endusre headless

--- runner.sh
+++ runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/galaxyproject/galaxy.git --branch release_20.09 --single-branch ./dist && cd dist/
-GALAXY_TEST_EXTERNAL=https://usegalaxy.org/ GALAXY_TEST_SELENIUM_HEADLESS=0 ./run_tests.sh -selenium lib/galaxy_test/selenium/test_history_multi_view.py:HistoryMultiViewTestCase.test_create_new_old_slides_next
+GALAXY_TEST_EXTERNAL=https://usegalaxy.org/ ./run_tests.sh -selenium lib/galaxy_test/selenium/test_history_multi_view.py:HistoryMultiViewTestCase.test_create_new_old_slides_next
