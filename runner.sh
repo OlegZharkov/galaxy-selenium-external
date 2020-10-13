9d6d07eb2ae31ad10241365ff3d76cb75c5744b7
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-13 18:24:14 +0200

    use eu

--- runner.sh
+++ runner.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://github.com/galaxyproject/galaxy.git --branch release_20.09 --single-branch ./dist && cd dist/
-GALAXY_TEST_EXTERNAL=https://usegalaxy.org/ ./run_tests.sh -selenium lib/galaxy_test/selenium/test_history_multi_view.py:HistoryMultiViewTestCase.test_create_new_old_slides_next
+#GALAXY_TEST_EXTERNAL=https://usegalaxy.org/ ./run_tests.sh -selenium lib/galaxy_test/selenium/test_history_multi_view.py:HistoryMultiViewTestCase.test_create_new_old_slides_next
+GALAXY_TEST_EXTERNAL=https://usegalaxy.eu/ ./run_tests.sh -selenium lib/galaxy_test/selenium/test_uploads.py:UploadsTestCase.test_upload_simplest
