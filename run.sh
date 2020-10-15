a3a0b27fb4ff4e96a6421ec41560d7afa62ecaa8
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-15 18:51:11 +0200

    curl from github action

--- run.sh
+++ run.sh
@@ -5,11 +5,3 @@ python -m venv galaxy/.venv
 source "galaxy/.venv/bin/activate" # activate virtualenv
 pip install -r galaxy/lib/galaxy/dependencies/dev-requirements.txt && pip install -r galaxy/requirements.txt
 python test_parser.py
-
-curl --location --request POST --url 'https://api.github.com/repos/OlegZharkov/galaxy-selenium-external/dispatches' --header 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' --header 'Accept: application/vnd.github.everest-preview+json' --header 'Content-Type: application/json' --data-raw '{
-  "event_type": "test-repository-dispatch",
-  "client_payload": {
-    "unit": false,
-    "integration": true
-  }
-}'
