c8e61fdf9210eb300a1b04c4b9d4e163410e7207
Oleg Zharkov <oleg.zharkov22@gmail.com>
2020-10-19 20:01:37 +0200

    debug

--- test_parser.py
+++ test_parser.py
@@ -43,9 +43,9 @@ def get_individual_tests(test_file_path):
         if selenium_test_dir in line:
             parts = line.split("::")
             test_name = f'{parts[1]}.{parts[2]}'
-            test_path = f'{parts[0]}:{test_name}'
+            test_path = f'{test_name}'
 
-            current_available_tests.append({"project": test_name, "test_path": test_path})
+            current_available_tests.append({"project": test_name, "config": test_path})
 
     return current_available_tests
 
